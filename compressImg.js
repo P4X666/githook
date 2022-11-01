565b828839a3199671044e008c158a335bc169fb
pengshixing <2644378911@qq.com>
2022-11-01 23:05:22 +0800

    增加日志

--- compressImg.js
+++ compressImg.js
@@ -4,38 +4,52 @@ const TINIFYKEY = "8tDFVWHMZqj02PpFfnBXq82hqrDKyz0B";
 const fs = require("fs");
 const path = require("path");
 const execSync = require("child_process").execSync; //同步子进程
-
-execSync(`git add .`);
-execSync(`git commit -m "auto commit message"`);
-return;
+const os = require("os");
 
 /** 获取文件大小 */
 const getSize = (path) => fs.statSync(path).size / 1024;
+/** 记录日志 */
+const recordText = (recordData) => {
+  fs.appendFile("compressRecord.txt", recordData, (err) => {
+    // 追加失败
+    if (err) throw err;
+    // 追加成功
+    console.log("追加内容成功！");
+  });
+};
 
 const name = execSync("git show -s --format=%cn").toString().trim(); //姓名
 const email = execSync("git show -s --format=%ce").toString().trim(); //邮箱
 const diffContent = execSync("git log -p -1").toString().trim(); //diff后的内容
 
-if(!diffContent){
+if (!diffContent) {
   console.log("没有 diff 内容");
   return;
 }
 
-// 自动提交的容错处理
-if(diffContent.includes("auto commit message")){
-  const autoMessage = diffContent.match(/(?<=auto commit message)(.+)(?=replace image end;)/g);
-  console.log("autoMessage: ", autoMessage);
+// 自动提交的容错处理 防止重复提交
+if (diffContent.includes("auto commit message by")) {
+  const autoMessage = diffContent.match(
+    /(?<=auto commit message by)(.+)(?=replace image end)/g
+  );
+  const [userInfo] = autoMessage;
+  const [userName, userEmail] = userInfo.split("::");
+  console.log("userName:", userName, "userEmail:", userEmail);
+  if (userName.trim() === name) {
+    console.log("该用户之前已经自动提交过了");
+  }
   return;
 }
 
-console.log("diff 内容",diffContent);
-
 /** 获取新增的 png jpg 图片 */
-const result = diffContent
-  .match(/(?<=Binary files)(.+)(?=differ)/g)
-  ?.map((item) => item?.includes(" and ") ? item.split(" and ")[1].trim() : '')
-  ?.filter((item) => item.includes("jpg") || item.includes("png"))
-  ?.map((item) => item.slice(1)) ?? [];
+const result =
+  diffContent
+    .match(/(?<=Binary files)(.+)(?=differ)/g)
+    ?.map((item) =>
+      item?.includes(" and ") ? item.split(" and ")[1].trim() : ""
+    )
+    ?.filter((item) => item.includes("jpg") || item.includes("png"))
+    ?.map((item) => item.slice(1)) ?? [];
 
 if (result.length === 0) {
   console.log("此次没有图片更新");
@@ -46,8 +60,7 @@ const tinify = require("tinify");
 tinify.key = TINIFYKEY;
 
 const sourceArr = [];
-result.forEach(async(item) => {
-  console.log("item:", item);
+result.forEach(async (item) => {
   const imgPath = __dirname + item;
   const oldImgSize = getSize(imgPath);
   const source = tinify.fromFile(imgPath);
@@ -56,34 +69,35 @@ result.forEach(async(item) => {
   sourceArr.push([source, imgPath, item, oldImgSize]);
 });
 
-Promise.all(sourceArr.map(([source, imgPath]) => source.toFile(imgPath)))
-  .then(
+Promise.all(sourceArr.map(([source, imgPath]) => source.toFile(imgPath))).then(
   (values) => {
     if (values.every((item) => item === undefined)) {
-      console.log("==============");
-      console.log("replace success");
-      console.log("==============");
+      let recordData = "";
       const filePath = sourceArr.reduce(
-        (preItem, [, , cur]) => {
-          console.log("cur：", preItem, cur);
+        (preItem, [, imgPath, cur, oldImgSize]) => {
+          // 记录图片大小变化
+          const newImgSize = getSize(imgPath);
+          const diffSize = (newImgSize - oldImgSize).toFixed(1);
+          recordData += `.${cur} 压缩之前大小：${oldImgSize}，压缩后的大小：${newImgSize}，缩小了约${diffSize}kb ${os.EOL}`;
+          // 第一个不做处理
           if (preItem) {
-            const [, imgPath, pre, oldImgSize] = preItem;
-            const newImgSize = getSize(imgPath);
-            const diffSize = (newImgSize - oldImgSize).toFixed(1);
-            console.log(
-              `图片压缩之前大小：${oldImgSize}，图片压缩后的大小：${newImgSize}，缩小了约${diffSize}kb`
-            );
+            const [, , pre] = preItem;
             return `.${pre} .${cur}`;
           }
           return `.${cur}`;
         },
         ""
       );
-      console.log("filePath:", filePath);
+      recordText(recordData);
       execSync(`git add ${filePath}`);
-      // setTimeout(() => {
-      //   execSync(`git commit -m "auto commit message"`);
-      // }, 500);
+      execSync(
+        `git commit -m "auto commit message by ${name} :: ${email} replace image end"`
+      );
+    } else {
+      recordText(`
+      压缩失败：${os.EOL}
+        ${sourceArr.map(([, , relativePath]) => relativePath+",")}
+      `);
     }
   }
 );
